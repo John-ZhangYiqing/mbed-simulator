828d43c6effcf34ed6b4a5e1fe1f470fb916e5ba
John Zhang <yiqzhang3@gmail.com>
2022-02-28 14:44:41 +0800

    Update launch-server.js

--- server/launch-server.js
+++ server/launch-server.js
@@ -17,7 +17,7 @@ const version = JSON.parse(fs.readFileSync(Path.join(__dirname, '..', 'package.j
 const compression = require('compression');
 
 const LORA_PORT = process.env.LORA_PORT || 1700;
-const LORA_HOST = process.env.LORA_HOST || 'router.eu.thethings.network';
+const LORA_HOST = process.env.LORA_HOST || 'eu1.cloud.thethings.network';
 
 let startupTs = Date.now();
 
